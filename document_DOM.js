1e5a1396344b7f254717d19f2a62ace0c54640ff
cutehumanS2 <ganzi_girl22@naver.com>
2021-02-05 19:48:02 +0900

    Document, DOM

---
+++ document_DOM.js
@@ -0,0 +1,10 @@
+/*
+//dir: 객체 형식을 볼 수 있음
+<--console.dir(document)-->
+-window.document 또는 window[document'] 또는 document를 쳤을 때는
+html 태그들이 나왔지만, 이번에는 객체 형식들이 나옴
+
+<--DOM 객체-->
+: Document Object Model
+-html과 js의 통역사 역할
+*/
